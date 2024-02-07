77c303bed72bc7e0226e1ed955f00751561a977d
dileesha abilash <dileeshabilash@gmail.com>
2024-02-07 11:16:14 +0530

    2/7 working

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, Button, Modal, TouchableOpacity,TextInput } from 'react-native';
+import { View, Text, Button, Modal, TouchableOpacity,TextInput ,FlatList,ActivityIndicator} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import tw from 'twrnc';
@@ -10,14 +10,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 var response2 = "hacker"
 const Stack = createStackNavigator();
+
+const Done = ()=>{
+return(
+
+<View >
+  <Text style={tw`text-2xl`}>
+    DONE
+  </Text>
+  </View>)
+}
+
 function App() {
+  
   return (
     <NavigationContainer>
   <Stack.Navigator>  
     <Stack.Screen name="Home" component={Main} />  
-    <Stack.Screen name="Pass">
-  {props => <Pass {...props} response={response} />}
-</Stack.Screen>
+    <Stack.Screen name="Pass" component={Pass}/>
+  
     {/* <Stack.Screen name="Notifications" component={Notifications} /> */}
   </Stack.Navigator>
 </NavigationContainer>
@@ -56,13 +67,95 @@ const Main = () => {
     { key: '1', value: 'COMBINED MATHEMATICS' },
     { key: '2', value: 'BUDDHISM' },
     { key: '3', value: 'HINDUISM' },
-  ];
+    { key: '4', value: 'CHRISTIANITY' },
+    { key: '5', value: 'ISLAM' },
+    { key: '6', value: 'BUDDHIST CIVILIZATION' },
+    { key: '7', value: 'HINDU CIVILIZATION' },
+    { key: '8', value: 'ISLAMIC CIVILIZATION' },
+    { key: '9', value: 'CHRISTIAN CIVILIZATION' },
+    { key: '10', value: 'BIOLOGY' },
+    { key: '11', value: 'ECONOMICS' },
+    { key: '12', value: 'ART' },
+    { key: '13', value: 'DANCING (INDIGENOUS)' },
+    { key: '14', value: 'DANCING (BHARATHA)' },
+    { key: '15', value: 'ORIENTAL MUSIC' },
+    { key: '16', value: 'CARNATIC MUSIC' },
+    { key: '17', value: 'WESTERN MUSIC' },
+    { key: '18', value: 'LOGIC & SCIENTIFIC METHOD' },
+    { key: '19', value: 'ENGINEERING TECHNOLOGY' },
+    { key: '20', value: 'BIO SYSTEMS TECHNOLOGY' },
+    { key: '21', value: 'BUSINESS STUDIES' },
+    { key: '22', value: 'AGRICULTURAL SCIENCE' },
+    { key: '23', value: 'GERMAN' },
+    { key: '24', value: 'PHYSICS' },
+    { key: '25', value: 'PALI' },
+    { key: '26', value: 'SINHALA' },
+    { key: '27', value: 'TAMIL' },
+    { key: '28', value: 'CIVIL TECHNOLOGY' },
+    { key: '29', value: 'MECHANICAL TECHNOLOGY' },
+    { key: '30', value: 'ELECTRICAL, ELECTRONIC AND INFORMATION TECHNOLOGY' },
+    { key: '31', value: 'FOOD TECHNOLOGY' },
+    { key: '32', value: 'AGRO TECHNOLOGY' },
+    { key: '33', value: 'BIO RESOURCE TECHNOLOGY' },
+    { key: '34', value: 'BUSINESS STATISTICS' },
+    { key: '35', value: 'SANSKRIT' },
+    { key: '36', value: 'CHEMISTRY' },
+    { key: '37', value: 'SCIENCE FOR TECHNOLOGY' },
+    { key: '38', value: 'DRAMA & THEATRE (SINAHALA)' },
+    { key: '39', value: 'DRAMA & THEATRE (TAMIL)' },
+    { key: '40', value: 'DRAMA & THEATRE (ENGLISH)' },
+    { key: '41', value: 'POLITICAL SCIENCE' },
+    { key: '42', value: 'FRENCH' },
+    { key: '43', value: 'ENGLISH' },
+    { key: '44', value: 'HOME ECONOMICS' },
+    { key: '45', value: 'ACCOUNTING' },
+    { key: '46', value: 'HISTORY OF INDIA' },
+    { key: '47', value: 'HISTORY OF EUROPE' },
+    { key: '48', value: 'HISTORY OF MODERN WORLD' },
+    { key: '49', value: 'ARABIC' },
+    { key: '50', value: 'MALAY' },
+    { key: '51', value: 'CHINESE' },
+    { key: '52', value: 'INFORMATION & COMMUNICATION TECHNOLOGY' },
+    { key: '53', value: 'HISTORY OF SRI LANKA' },
+    { key: '54', value: 'HINDI' },
+    { key: '55', value: 'KOREAN' },
+    { key: '56', value: 'MATHEMATICS' },
+    { key: '57', value: 'HIGHER MATHEMATICS' },
+    { key: '58', value: 'RUSSIAN' },
+    { key: '59', value: 'GREEK AND ROMAN CIVILIZATION' },
+    { key: '60', value: 'JAPANESE' },
+    { key: '61', value: 'GEOGRAPHY' },
+    { key: '62', value: 'COMMUNICATION & MEDIA STUDIES' }
+];
 
-  const locationsData = [
-    { key: '1', value: 'COLOMBO' },
-    { key: '2', value: 'GAMPAHA' },
-    { key: '3', value: 'KALUTARA' },
-  ];
+const locationsData = [
+  { key: '1', value: 'COLOMBO' },
+  { key: '2', value: 'GAMPAHA' },
+  { key: '3', value: 'KALUTARA' },
+  { key: '4', value: 'MATALE' },
+  { key: '5', value: 'KANDY' },
+  { key: '6', value: 'NUWARA ELIYA' },
+  { key: '7', value: 'GALLE' },
+  { key: '8', value: 'MATARA' },
+  { key: '9', value: 'HAMBANTOTA' },
+  { key: '10', value: 'JAFFNA' },
+  { key: '11', value: 'KILINOCHCHI' },
+  { key: '12', value: 'MANNAR' },
+  { key: '13', value: 'MULLAITIVU' },
+  { key: '14', value: 'VAVUNIYA' },
+  { key: '15', value: 'TRINCOMALEE' },
+  { key: '16', value: 'BATTICALOA' },
+  { key: '17', value: 'AMPARA' },
+  { key: '18', value: 'PUTTALAM' },
+  { key: '19', value: 'KURUNEGALA' },
+  { key: '20', value: 'ANURADHAPURA' },
+  { key: '21', value: 'POLONNARUWA' },
+  { key: '22', value: 'BADULLA' },
+  { key: '23', value: 'MONARAGALA' },
+  { key: '24', value: 'KEGALLE' },
+  { key: '25', value: 'RATNAPURA' },
+];
+
 
   const req = (navigation) => {
     setlsLoading(true);
@@ -70,11 +163,11 @@ const Main = () => {
       .then(res => res.json())
       .then(
         (result) => {
-          setlsLoading(false);
           setResponse(result);
           console.log(response2)
           response2 = result
           console.log(response2)
+          setlsLoading(false);
 
           navigation.navigate('Pass');
 
@@ -101,7 +194,15 @@ const Main = () => {
     }
   }, [response, error]);
   console.log(selected1,selected2,selected3,basi,district,zzz);
+  
+  if (isLoading==true){
+    return(
+    <ActivityIndicator size="large">
+    </ActivityIndicator>)
+  }
+  
   return (
+    
     <View style={{ flex: 1, flexDirection: 'column' }}>
       <View style={{  flex: 3 }}></View>
       
@@ -191,18 +292,22 @@ const Main = () => {
       {/* Modal 1 */}
       <Modal visible={modal1Visible}   animationType="slide" transparent={false}  onRequestClose={() => setModal1Visible(false)}>
        <View style={{ backgroundColor: 'black', flex:1}}> 
-        <View style={tw`flex-1 justify-center items-center m-8 border-2 rounded-xl bg-white`}>
+        <View style={tw`flex-1 justify-center items-center m-8 border-2 rounded-xl bg-white text-white`}>
           <Text>Modal 1 Content</Text>
           <SelectList 
         setSelected={(val) => setSelected1(val)} 
         data={data} 
         save="value"
-        style={{ color: 'white' }}
+        style={tw`text-white`}
         
     />
           <TouchableOpacity onPress={() => setModal1Visible(false)}>
-            <Text>Close Modal 1</Text>
+            <View style={tw`bg-emerald-400 top-10 p-5 rounded-3xl`}>
+        
+            <Done />
+            </View>
           </TouchableOpacity>
+        
         </View>
         </View>
 
@@ -223,7 +328,10 @@ const Main = () => {
         
     />
           <TouchableOpacity onPress={() => setModal2Visible(false)}>
-            <Text>Close Modal 2</Text>
+          <View style={tw`bg-emerald-400 top-10 p-5 rounded-3xl`}>
+        
+        <Done />
+        </View>
           </TouchableOpacity>
         </View>
         </View>
@@ -245,7 +353,10 @@ const Main = () => {
         
     />
           <TouchableOpacity onPress={() => setModal3Visible(false)}>
-            <Text>Close Modal 3</Text>
+          <View style={tw`bg-emerald-400 top-10 p-5 rounded-3xl`}>
+        
+        <Done />
+        </View>
           </TouchableOpacity>
         </View>
         </View>
@@ -268,7 +379,10 @@ const Main = () => {
         
     />
           <TouchableOpacity onPress={() => setModal4Visible(false)}>
-            <Text>Close Modal 4</Text>
+          <View style={tw`bg-emerald-400 top-10 p-5 rounded-3xl`}>
+        
+        <Done />
+        </View>
           </TouchableOpacity>
         </View>
         </View>
@@ -285,13 +399,16 @@ const Main = () => {
           <Text>Modal 4 Content</Text>
           
           <TextInput placeholder='   z-score'
-    style={{ borderWidth: 2, width: 90, color: 'white', borderColor: 'white' }}
+    style={{ borderWidth: 2, width: 90, color: 'black', borderColor: 'white',	backgroundColor:"#faffff" ,padding:10}}
     value={zzz}
     onChangeText={setZed}
     keyboardType='phone-pad' />
    
           <TouchableOpacity onPress={() => setModalz(false)}>
-            <Text>Close Modal 4</Text>
+          <View style={tw`bg-emerald-400 top-10 p-5 rounded-3xl`}>
+        
+        <Done />
+        </View>
           </TouchableOpacity>
         </View>
         </View>
@@ -314,7 +431,10 @@ const Main = () => {
     keyboardType='phone-pad' />
    
           <TouchableOpacity onPress={() => setModalbais(false)}>
-            <Text>Close Modal 4</Text>
+          <View style={tw`bg-emerald-400 top-10 p-5 rounded-3xl`}>
+        
+        <Done />
+        </View>
           </TouchableOpacity>
         </View>
         </View>
@@ -333,10 +453,21 @@ const Main = () => {
 };
 
 const Pass = () => {
- 
+  const renderItem = ({ item }) => (
+    <View style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: '#ccc' }}>
+      <Text style={{ fontWeight: 'bold' }}>Category: {item.category}</Text>
+      <Text>University: {item.university}</Text>
+      <Text>Z-Score: {item['z-score']}</Text>
+    </View>
+  );
+
   return (
     <View>
-      <Text style={{ fontSize: 50 }}>hi {response2}</Text>
+      <FlatList
+      data={response2}
+      renderItem={renderItem}
+      keyExtractor={(item, index) => index.toString()}
+    />
     </View>
   );
 };
