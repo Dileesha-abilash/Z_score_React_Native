0cffebd7d93d63bb873fd460c18f4032f6f6dbae
dileesha abilash <943652194+Dileesha-abilash@users.noreply.github.com>
2024-04-05 21:44:45 +0530

    before spash

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler';
+import * as SplashScreen from 'expo-splash-screen';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,6 +8,8 @@ import My_Stack from './screens/My_stack';
 import Tab2 from './screens/Tab2';
 import Tab3 from './screens/Tab3';
 import Tab4 from './screens/tab4';
+
+
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -28,9 +31,9 @@ const MainScreen = () => {
         <Tab.Navigator screenOptions={{
             headerShown: false // This will hide all headers in the stack
         }}>
-            <Tab.Screen name="Stack" component={My_Stack} title="course"/>
-            <Tab.Screen name="Tab2" component={Tab2} />
+            <Tab.Screen name="Stack" component={My_Stack} />
             <Tab.Screen name="Tab3" component={Tab3} />
+            <Tab.Screen name="Tab2" component={Tab2} />
             <Tab.Screen name="Tab4" component={Tab4} />
         </Tab.Navigator>
     );
